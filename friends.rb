b775ea51e56a9e7b9974bceaf7fcf302ef3a0480
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-08 15:21:13 -0700

    Add destinations

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ arrays = ["Grant", "Cat", "Sarah", "Zoe", "Matt"]
 puts arrays [2]
 puts arrays[4]
 puts arrays[0]
+
+destinations = ["Bali", "Australia", "India", "Greenland", "Norway"]
+
+puts destinations.push("Egypt")
